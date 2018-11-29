2bef5ba39ba9e17891bb803d05987607ada48613
msec-kul <msec_kul@hotmail.com>
2018-11-29 09:21:04 +0100

    Initial commit

---
+++ app/src/main/java/be/kuleuven/msec/iot/myapplication/ApplicationEnvironment.java
@@ -0,0 +1,123 @@
+package be.kuleuven.msec.iot.myapplication;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+import be.kuleuven.msec.iot.iotframework.callbackinterfaces.OnRequestCompleted;
+import be.kuleuven.msec.iot.iotframework.generic.componentlayer.Environment;
+import be.kuleuven.msec.iot.iotframework.generic.devicelayer.HumiditySensor;
+import be.kuleuven.msec.iot.iotframework.generic.devicelayer.Lamp;
+import be.kuleuven.msec.iot.iotframework.generic.devicelayer.PressureSensor;
+import be.kuleuven.msec.iot.iotframework.generic.devicelayer.TemperatureSensor;
+import be.kuleuven.msec.iot.iotframework.generic.devicelayer.VirtualIoTConnector;
+import be.kuleuven.msec.iot.iotframework.generic.devicelayer.VirtualIoTDevice;
+import be.kuleuven.msec.iot.iotframework.implementations.lamps.huelamp.HueGateway;
+import be.kuleuven.msec.iot.iotframework.implementations.sensorkits.allthingstalk.AllThingsTalkGateway;
+import be.kuleuven.msec.iot.iotframework.implementations.sensorkits.versasense.VersaSenseGateway;
+import be.kuleuven.msec.iot.iotframework.systemmanagement.constants.Connector_constants;
+import be.kuleuven.msec.iot.iotframework.systemmanagement.constants.Device_constants;
+import be.kuleuven.msec.iot.iotframework.systemmanagement.jsonmodel.JSMConnector;
+import be.kuleuven.msec.iot.iotframework.systemmanagement.jsonmodel.JSMDevice;
+
+/**
+ * Created by ilsebohe on 16/01/2018.
+ */
+
+public class ApplicationEnvironment extends Environment {
+
+    final private String TAG = "ApplicationEnvironment";
+    Environment thisEnvironment;
+
+    private static List<VirtualIoTDevice> devices;
+
+    private Context context;
+
+    public ApplicationEnvironment(Context context) {
+        super();
+        this.thisEnvironment=this;
+        this.context = context;
+        devices = new ArrayList<VirtualIoTDevice>();
+        //room = new Room("Room");
+    }
+    public List<VirtualIoTDevice> getDevices(){
+        return devices;
+    };
+
+    public VirtualIoTDevice getDeviceBySystemID(String systemID){
+        for (VirtualIoTDevice dev: devices
+             ) {
+            if (dev.getSystemID().equals(systemID)) return dev;
+
+        }
+        return null;
+    }
+
+    public void loadDevices(Context context, String filename, final OnRequestCompleted orc ){
+        this.getConfigurationFromServer(context, filename, new OnRequestCompleted<Boolean>() {
+            @Override
+            public void onSuccess(Boolean aBoolean) {
+                thisEnvironment.loadEnvironment(new OnRequestCompleted<Boolean>() {
+                    @Override
+                    public void onSuccess(Boolean aBoolean) {
+                        orc.onSuccess(true);
+                    }
+                });
+            }
+        });
+    }
+
+
+    @Override
+    public void loadEnvironment(final OnRequestCompleted<Boolean> orc) {
+
+        final CountDownLatch latch = new CountDownLatch(configuration.getConnectors().size());
+        System.out.println("number of connectors "+latch.getCount());
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //initializeConnectors
+                for (JSMConnector connector : configuration.getConnectors()) {
+                    switch (connector.getType()) {
+                        case Connector_constants.CONNECTORTYPE_HUE:
+                            virtualIoTConnectors.add(new HueGateway(connector.getSystemID(), connector.getSettings()));
+                            break;
+                        case Connector_constants.CONNECTORTYPE_ALL_THINGS_TALK:
+                            virtualIoTConnectors.add(new AllThingsTalkGateway(connector.getSystemID(), connector.getSettings()));
+                            break;
+                        case Connector_constants.CONNECTORTYPE_VERSASENSE:
+                            virtualIoTConnectors.add(new VersaSenseGateway(connector.getSystemID(), connector.getSettings()));
+                            break;
+                    }
+                }
+                for (final VirtualIoTConnector c : virtualIoTConnectors) {
+                    c.initialize(new OnRequestCompleted() {
+                        @Override
+                        public void onSuccess(Object response) {
+                            c.updateConnectedDeviceList(new OnRequestCompleted<Boolean>() {
+                                @Override
+                                public void onSuccess(Boolean response) {
+                                    latch.countDown();
+                                    System.out.println("SUCCES UPDATE "+c.getSystemID());
+                                }
+                            }, configuration.getComponents().get(0).getDevices());
+                        }
+                    });
+                }
+                try {
+                    latch.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                for (JSMDevice d : configuration.getComponents().get(0).getDevices()) {
+                    System.out.println("devices" + devices + " " + d.getConnector() +  " " + getConnectorBySystemID(d.getConnector()) );
+                    devices.add(getConnectorBySystemID(d.getConnector()).getConnectedDeviceBasedOnSystemID(d.getSystemID()));
+                }
+                orc.onSuccess(true);
+            }
+        }).start();
+    }
+}
---
+++ app/src/main/java/be/kuleuven/msec/iot/myapplication/MainActivity.java
@@ -0,0 +1,75 @@
+package be.kuleuven.msec.iot.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import be.kuleuven.msec.iot.iotframework.callbackinterfaces.OnEventOccurred;
+import be.kuleuven.msec.iot.iotframework.callbackinterfaces.OnRequestCompleted;
+import be.kuleuven.msec.iot.iotframework.generic.devicelayer.Lamp;
+import be.kuleuven.msec.iot.iotframework.generic.devicelayer.PressureSensor;
+import be.kuleuven.msec.iot.iotframework.generic.devicelayer.TemperatureSensor;
+
+public class MainActivity extends AppCompatActivity {
+
+    Lamp lamp1;
+    TemperatureSensor temp1;
+    PressureSensor pres1;
+
+
+
+    TextView temperatureTextView;
+    TextView pressureTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        temperatureTextView = findViewById(R.id.temperature_textview);
+        pressureTextView = findViewById(R.id.pressure_textview);
+
+        final ApplicationEnvironment e = new ApplicationEnvironment(this);
+        e.loadDevices(this, "configurations.json", new OnRequestCompleted() {
+           @Override
+           public void onSuccess(Object o) {
+               lamp1 = (Lamp) e.getDeviceBySystemID("LAMP1");
+               temp1 = (TemperatureSensor) e.getDeviceBySystemID("TEMP1");
+               pres1 = (PressureSensor) e.getDeviceBySystemID("PRES1");
+
+               temp1.monitorTemperature(new OnEventOccurred<Double>() {
+                   @Override
+                   public void onUpdate(Double aDouble) {
+                       temperatureTextView.setText("TEMPERATURE: "+ aDouble + "°C");
+                   }
+               });
+               pres1.monitorPressure(new OnEventOccurred<Double>() {
+                   @Override
+                   public void onUpdate(Double aDouble) {
+                       pressureTextView.setText("PRESSURE: " + aDouble+"");
+                   }
+               });
+           }
+       });
+
+
+    }
+
+    public void buttonOnPush(View view) {
+        lamp1.turnOn(new OnRequestCompleted<Boolean>() {
+            @Override
+            public void onSuccess(Boolean aBoolean) {
+            }
+        });
+    }
+    public void buttonOffPush(View view) {
+        lamp1.turnOff(new OnRequestCompleted<Boolean>() {
+            @Override
+            public void onSuccess(Boolean aBoolean) {
+            }
+        });
+    }
+}
